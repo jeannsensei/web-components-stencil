c8919efb0a6f3fe612f9c7da1c15235c7ca7b1a6
Jeannuel Garcia <jjeanlink@gmail.com>
2021-08-03 10:05:03 -0500

    Section 7 - 95. Using State

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,72 @@
+import { Component, h, Prop, State } from '@stencil/core';
+
+@Component({
+  tag: 'uc-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true,
+})
+export class SideDrawer {
+  @State() showContactInfo: boolean = false;
+
+  @Prop({ reflect: true }) sidebar_title: string;
+  @Prop({ reflect: true, mutable: true }) open: boolean;
+
+  onCloseDrawer() {
+    this.open = false;
+  }
+
+  onContentChange(content: string) {
+    this.showContactInfo = content === 'contact';
+  }
+
+  render() {
+    // let content = null;
+    // if (this.open) {
+    //   content = (
+    //     <aside>
+    //       <header>
+    //         <h1>{this.sidebar_title}</h1>
+    //       </header>
+    //       <main>
+    //         <slot />
+    //       </main>
+    //     </aside>
+    //   );
+    // }
+
+    let mainContent = <slot />;
+    if (this.showContactInfo) {
+      mainContent = (
+        <div id="contact-information">
+          <h2>Contact Information</h2>
+          <p>You can reach us via phone or email.</p>
+
+          <ul>
+            <li>Phone: 12312321</li>
+            <li>
+              E-mail: <a href="mailto:something@something.com">something@something.com</a>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+
+    return (
+      <aside>
+        <header>
+          <h1>{this.sidebar_title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+        </header>
+        <section id="tabs">
+          <button class={!this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'nav')}>
+            Navigation
+          </button>
+          <button class={this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'contact')}>
+            Contact
+          </button>
+        </section>
+        <main>{mainContent}</main>
+      </aside>
+    );
+  }
+}
